20d17a3386ce508a3d090a21a7dac5b30e1c75f1
Abhay Deomane <88963060+Abhaydeomane@users.noreply.github.com>
2024-02-06 12:43:55 +0530

    Add files via upload

---
+++ frontend/frontend.js
@@ -0,0 +1,41 @@
+function calculateTotalPrice() {
+    const zone = document.getElementById('zone').value;
+    const organization_id = document.getElementById('organization_id').value;
+    const total_distance = document.getElementById('total_distance').value;
+    const item_type = document.getElementById('item_type').value;
+
+    const requestData = {
+        zone: zone,
+        organization_id: organization_id,
+        total_distance: parseFloat(total_distance),
+        item_type: item_type
+    };
+
+    // Check if any of the input fields are empty
+    if (!zone || !organization_id || !total_distance || !item_type) {
+        alert('Missing required parameters');
+        return;
+    }
+
+    // Show loading spinner
+    document.getElementById('loading').style.display = 'block';
+
+    fetch('https://food-price-api.onrender.com/cost', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Hide loading spinner
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('total_price').innerText = `Total Price: ${data.total_price}`;
+    })
+    .catch(error => {
+        // Hide loading spinner
+        document.getElementById('loading').style.display = 'none';
+        document.getElementById('total_price').innerText = `Error`;
+    });
+}
